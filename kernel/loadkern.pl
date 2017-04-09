92f615fb5bcf6eb599f024b22fc5b1b01f2a44c4
VivianSZF <903498500@qq.com>
2017-04-09 19:09:43 +0800

    fix a bug in loadkern.pl and add the answers to the questions in the report

--- kernel/loadkern.pl
+++ kernel/loadkern.pl
@@ -2,15 +2,15 @@
 
 open(SIG, $ARGV[0]) || die "open $ARGV[0]: $!";
 
-$n = sysread(SIG, $buf, 100*1024);
+$n = sysread(SIG, $buf, 200*1024);
 
-if($n > 100 * 1024){
+if($n > 200 * 1024){
   print STDERR "ERROR:Kernel binary file too large:\
-	  $n bytes (max 102400)\n";
+	  $n bytes (max 204800)\n";
   exit 1;
 }
 
-print STDERR "Kernel binary file is $n bytes (max 102400)\n";
+print STDERR "Kernel binary file is $n bytes (max 204800)\n";
 
 $m = $n % (512);
 $s = 512 - $m;
@@ -32,12 +32,12 @@ open(SIG, $ARGV[0]) || die "open $ARGV[0]: $!";
 
 $n = sysread(SIG, $buf, 200*1024);
 
-if($n > 100 * 1024){
-  print STDERR "ERROR:Kernel binary file large: $n bytes (max 102400)\n";
+if($n > 200 * 1024){
+  print STDERR "ERROR:Kernel binary file large: $n bytes (max 204800)\n";
   exit 1;
 }
 
-print STDERR "Kernel binary file is $n bytes (max 102400)\n";
+print STDERR "Kernel binary file is $n bytes (max 204800)\n";
 print STDERR "*********************************************\n";
 print STDERR "*********************************************\n";
 print STDERR "*********************************************\n";
